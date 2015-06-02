5e976f9b93cf64c03f82bd545fb863d686832cc9
Sriram <sriru1998@gmail.com>
2015-06-02 13:57:39 +0530

    Finished. Working.
    Ranking system done.
    Design done. 
    Fully functional.
    Copy and paste in a new file!
    Will make tc edits and upload later!

--- schoolreportcard.cpp
+++ schoolreportcard.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 short int i;
 class student {
-float unit1[5], unit2[5], terminal[5], tu1, tu2, tt, tp;
+float unit1[5], unit2[5], terminal[5], tu1, tu2, tt;
 public:
+     float total;
      void input(){
           //Inputing Unit 1
 for(i=0;i<80;i++)cout<<"x";
@@ -83,13 +84,13 @@ for(i=0;i<80;i++)cout<<"x";
 for(i=0;i<80;i++)cout<<"x";
 }
      void compute(){
-          tu1=tu2=tt=tp=0;
+          tu1=tu2=tt=total=0;
           for(i=0;i<5;i++) tu1+=unit1[i];
           for(i=0;i<5;i++) tu2+=unit2[i];
           for(i=0;i<5;i++) tt+=terminal[i];
-          tp=((tu1+tu2)/250*20/100)+(tt/500*80/100);
-          tp*=100;
-          cout<<"Consolidated Report = "<<tp<<endl;
+          total=((tu1+tu2)/250*20/100)+(tt/500*80/100);
+          total*=100;
+          cout<<"Consolidated Report = "<<total<<endl;
      }
 };
 int main()
@@ -102,9 +103,34 @@ int main()
      cout<<" ---> ENTER THE NUMBER OF STUDENTS: ";
      cin>>nos;
      student s[nos];
-     for(i=0;i<nos;i++) {
-          cout<<"\nSTUDENT "<<i+1<<" DETAILS:"<<endl;
-          s[i].input();
+     int k;
+     for(k=0;k<nos;k++) {
+          cout<<"\nSTUDENT "<<k+1<<" DETAILS:"<<endl;
+          s[k].input();
      }
+     // hope you had understood till this part ^
+     //now for the sorting the marks for the ranks
+     int r[10]; // this is the array containing the list of students in the order of ascending marks
+     for(i=0;i<nos;i++) //we are assuming 1 2 3... by default!
+          r[i]=i;
+     int j,key; //variables needed for the sorting algo
+     //this algorithm is called the insertion sort
+     for(j=1;j<nos;j++){
+          key=r[j]; //we are gonna use this key variable to compare to the values below it and sort it
+          i=j-1; //i is gonna be the changing variable which will be used to compare with the key
+          while(i>=0 && s[r[i]].total>s[key].total){
+               r[i+1]=r[i];                            // complicated stuff i created i will explain later in person :3
+               i--;
+          }
+          r[i+1]=key;
+     }
+     //now r array contains the id of students sorted acc to mark!
+     //we just have to output students mark acc to r array
+     for(i=0;i<40;i++) cout<<"~ ";
+     cout<<"\t\t\t\***RANKS*** ";
+     for(i=0;i<40;i++) cout<<"~ ";
+     cout<<endl;
+     for(i=nos-1, j=1;i>=0;i--,j++) cout<<"Rank "<<j<<" : "<<"Student "<<r[i]+1<<" Consolidated marks: "<<s[r[i]].total<<endl;
+
      return 0;
 }
